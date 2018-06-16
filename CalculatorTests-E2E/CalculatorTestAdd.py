6f492eda7ac41a53eea87cb577d09ae5173bede7
Edson Cruz <edsonclj@gmail.com>
2018-06-16 07:31:20 -0300

    added comments to test class

--- CalculatorTests-E2E/CalculatorTestAdd.py
+++ CalculatorTests-E2E/CalculatorTestAdd.py
@@ -3,16 +3,26 @@ import time
 import selenium.webdriver.support.ui as UI
 
 driver = webdriver.Chrome()
-# Open the webpage
+# Open the desired web page
 driver.get('http://juliemr.github.io/protractor-demo/')
 
+# Type in numbers for operation
 first_number = driver.find_element_by_css_selector('[ng-model=first]')
-first_number.send_keys('2')
+first_number.send_keys('25')
 second_number = driver.find_element_by_css_selector('[ng-model=second]')
-second_number.send_keys('3')
+second_number.send_keys('42')
+
+# Select the operation to be performed
 operation_add = UI.Select(driver.find_element_by_css_selector('[ng-model=operator]')).select_by_value("ADDITION")
+
+# Click on the button to perform operation
 result_button = driver.find_element_by_class_name('btn')
 result_button.click()
-result = driver.find_elements_by_tag_name('h2')
-time.sleep(5)
-assert result == '5'
+
+# Await operation to be performed
+time.sleep(4)
+result = driver.find_element_by_class_name('ng-binding')
+
+# Assert operation result and finish browser instance
+assert result.text == '67'
+driver.quit()
