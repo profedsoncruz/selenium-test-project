4c87d64a16712dfc6d5f6d451a50441019b8cf01
Professor Edson Cruz <39575202+profedsoncruz@users.noreply.github.com>
2018-09-28 20:54:36 -0300

    Updated example file to avoid selenium loss of focus on page

--- selenium-test-project/ExampleTest.py
+++ selenium-test-project/ExampleTest.py
@@ -1,3 +1,4 @@
+import time
 from selenium import webdriver
 from page_objects import PageObject, PageElement
 
@@ -6,18 +7,25 @@ class Google(PageObject):
     search_bar = PageElement(id_='lst-ib')
     btn_search = PageElement(name='btnK')
     btn_lucky = PageElement(name='btnI')
+    g_logo = PageElement(id_='hplogo')
 
     def search(self, text):
         self.search_bar.send_keys(text)
+        self.g_logo.click()
         self.btn_search.click()
 
     def lucky(self, text):
         self.search_bar.send_keys(text)
+        self.g_logo.click()
         self.btn_lucky.click()
 
 
 browser = webdriver.Chrome()
-g = Google(browser, 'http://google.com/')
+g = Google(browser, 'http://www.google.com.br/')
 g.get('')
-# g.search('Universidade Positivo')
+time.sleep(3)
+g.search('Universidade Positivo')
+time.sleep(20)
+g.get('')
+time.sleep(3)
 g.lucky('Pós Graduação Testes de Software da Universidade Positivo')
